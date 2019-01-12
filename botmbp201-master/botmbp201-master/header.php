9efe8528ffe9989f636ad9fb3bfd8e0da52dadd9
androidyasmin <46198021+androidyasmin@users.noreply.github.com>
2019-01-12 16:26:06 +0300

    update

---
+++ botmbp201-master/botmbp201-master/header.php
@@ -0,0 +1,54 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>hey</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://getbootstrap.com/docs/4.1/examples/sticky-footer/sticky-footer.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.5/angular.min.js"></script>
+    <script src="js/main.js"></script>
+    <style>
+        .navbar-default{
+            background: #3498db;
+        }
+        .navbar-default a{ 
+            color: #ffF;
+        }
+        .footer{
+          background:#c0392b;
+        }
+        .text-muted{color:#fff !important;}
+    </style>
+</head>
+<body ng-app="q" ng-controller="qCtrl">
+    <nav class="navbar navbar-expand-lg navbar-default">
+        <a class="navbar-brand" href="#">Sohbet Robotu</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Anasayfa <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Diller</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Sohbet Kutusu</a>
+              </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
---
+++ botmbp201-master/botmbp201-master/index.php
@@ -0,0 +1,63 @@
+<?php require "header.php"; ?>
+<?php 
+      $handle = curl_init();
+ 
+      $url = "https://api.stackexchange.com/2.2/search?order=desc&sort=votes&tagged=php&site=stackoverflow";
+       
+
+      function get_web_page( $url )
+      {
+          $options = array(
+              CURLOPT_RETURNTRANSFER => true,     // return web page
+              CURLOPT_HEADER         => false,    // don't return headers
+              CURLOPT_FOLLOWLOCATION => true,     // follow redirects
+              CURLOPT_ENCODING       => "",       // handle all encodings
+              CURLOPT_USERAGENT      => "spider", // who am i
+              CURLOPT_AUTOREFERER    => true,     // set referer on redirect
+              CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
+              CURLOPT_TIMEOUT        => 120,      // timeout on response
+              CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
+              CURLOPT_SSL_VERIFYPEER => false     // Disabled SSL Cert checks
+          );
+      
+          $ch      = curl_init( $url );
+          curl_setopt_array( $ch, $options );
+          $content = curl_exec( $ch );
+          $err     = curl_errno( $ch );
+          $errmsg  = curl_error( $ch );
+          $header  = curl_getinfo( $ch );
+          curl_close( $ch );
+      
+
+          return $content;
+      }
+      $x = get_web_page($url);
+      $json = json_decode($x, true)['items'];
+      $i = 1;
+      ?>
+      <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">No</th>
+          <th scope="col">Dil</th>
+          <th scope="col">Soru</th>
+          <th scope="col">Cevap</th>
+        </tr>
+      </thead>
+      <tbody>
+          <?php 
+            foreach($json as $key => $val){
+              echo '
+                <tr>
+                  <th scope="row">'.$i.'</th>
+                  <td>PHP</td>
+                  <td>'.$val['title'].'</td>
+                  <td>--</td>
+                </tr>
+              ';
+              $i++;
+            }
+          ?>
+      </tbody>
+    </table>
+<?php require "footer.php"; ?>
